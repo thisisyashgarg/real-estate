a8f0b8fe5322a0874bfcaa1471ae9f4c303ce2d5
Yash Garg <thisisyashgarg@gmail.com>
2024-10-30 00:44:25 +0530

    feat: mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const userRouter = require("./routes/userRoute");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -12,27 +15,43 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+// mongoose connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/user', userRouter);
 app.use(fileUpload());
 
-// deployment
-__dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// Connect to the database
+connectDB().then(
+  ()=>{
+    __dirname = path.resolve();
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is Running! 🚀");
-  });
-}
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+      });
+    } else {
+      app.get("/", (req, res) => {
+        res.send("Server is Running! 🚀");
+      });
+    }
 
-// error middleware
-// app.use(errorMiddleware);
+      }
+).catch((error) => {
+  console.log(error);
+});
 
 module.exports = app;
